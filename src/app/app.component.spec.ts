55b073555b337f26c9e802c24d150d212e3ee7fe
jawad <j.saouti@rentasolutions.org>
2022-09-26 11:30:33 +0200

    add 5 unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('bowling-simulator');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('bowling-simulator app is running!');
-  });
 });
--- src/app/bowling-simulator/bowling-simulator.component.spec.ts
+++ src/app/bowling-simulator/bowling-simulator.component.spec.ts
@@ -20,4 +20,55 @@ describe('BowlingSimulatorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('functions', () => {
+    it('should empty the array pinsFallenCurrentRound when switchRounds() is called' , () => {
+      component.pinsFallenCurrentRound.length = 2;
+      component.pinsFallenCurrentRound = [5, 3]
+
+      fixture.detectChanges();
+
+      // @ts-ignore : we will not render this function public for testing
+      component.switchRounds();
+
+      expect(component.pinsFallenCurrentRound).toEqual([]);
+    });
+
+    it('should disable the play button when the game is finished' , () => {
+      component.totalThrows = 19;
+      const playBtn = fixture.debugElement.nativeElement.querySelector('#play-btn')
+
+      fixture.detectChanges();
+
+      // @ts-ignore : we will not render this function public for testing
+      component.manageEndOfTheGame();
+
+      expect(component.gameFinished).toEqual(true);
+      expect(playBtn.disabled).toBeTruthy();
+    });
+  });
+
+  describe('validators', () => {
+    it('should check the min validator of the pins input', () => {
+      // @ts-ignore
+      component.pinsNumber.setValue(-5);
+      fixture.detectChanges();
+      expect(component.pinsNumber.valid).toBeFalsy();
+    });
+
+    it('should check the max validator of the pins input', () => {
+      // @ts-ignore
+      component.pinsNumber.setValue(11);
+      fixture.detectChanges();
+      expect(component.pinsNumber.valid).toBeFalsy();
+    });
+
+    it('should check the roundRuleNotRespected validator of the pins input', () => {
+      // @ts-ignore
+      component.roundRuleNotRespected = true
+      fixture.detectChanges();
+      expect(component.pinsNumber.valid).toBeFalsy();
+    });
+  })
+
 });
